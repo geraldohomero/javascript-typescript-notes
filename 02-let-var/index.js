b1fcd48b8136ac7e1d515f7aba17ae91f4b8ea49
Geraldo Homero <geraldohomero@protonmail.com>
2023-07-12 17:51:19 -0300

    update

---
+++ 02-let-var/index.js
@@ -0,0 +1,10 @@
+// ECMAScript 2025 (ES6)
+
+//nome = 'Geraldo'; // N faça isso, pois irá criar uma variável global no código
+var nome = 'Geraldo';
+var sobrenome = 'Homero';
+
+let name = 'Gerald';
+let surname = 'Homer';
+
+console.log(nome, sobrenome)
