aba06139868b19a3f807463b43db4bfc5f5112b8
Geraldo Homero <geraldohomero@protonmail.com>
2023-07-12 17:45:37 -0300

    first commit

---
+++ imc-calc/inbdex.js
@@ -0,0 +1,22 @@
+/*
+Luiz Otávio Miranda tem 30 anos, pesa 84 kg
+tem 1.8 de altura e seu IMC é de 25.925925925925924
+Luiz Otávio nasceu em 1980
+*/
+const nome = 'Geraldo';
+const sobrenome = 'Homero';
+const idade = 28;
+const peso = 84;
+const alturaEmM = 1.80;
+let indiceMassaCorporal; // peso / (altura * altura)
+let anoNascimento;
+
+indiceMassaCorporal = peso / (alturaEmM * alturaEmM);
+anoNascimento = 2023 - idade;
+
+
+// template strings
+
+console.log(nome + ' ' + sobrenome + ' tem ' + idade + ' anos, pesa ' + peso + ' kg');
+console.log(`tem ${alturaEmM} de altura e seu IMC é de ${indiceMassaCorporal}`);
+console.log(`${nome} nasceu em ${anoNascimento}.`);
