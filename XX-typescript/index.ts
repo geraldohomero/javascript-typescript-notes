8081b52e6b5936fe8600807dc5b1e799f6b5b304
Geraldo Homero <geraldohomero@protonmail.com>
2023-07-13 14:23:55 +0000

    let message: string = 'Hello World'; console.log(message);

--- XX-typescript/index.ts
+++ XX-typescript/index.ts
@@ -1,2 +1,16 @@
-let message: string = 'Teste';
-console.log(message);
+class Person {
+    name: string;
+    age: number;
+  
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    greet(): void { 
+      console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+  }
+  
+  let alice = new Person("Alice", 25); 
+  alice.greet(); 
