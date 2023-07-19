c9dda716b5ccfe055faf4781c6e658c89f6f5185
Geraldo Homero <geraldohomero@protonmail.com>
2023-07-19 16:07:11 +0000

    add 13-objetos/index.js

---
+++ 13-objetos/index.js
@@ -0,0 +1,9 @@
+const array = [1, 2, 3, 4, 5];
+array.push(6);
+array[0] = 'João';
+//array = 'Outro nome'; // não pode ser feito, pois array é uma constante, com let pode ser feito
+console.log(array);
+
+const nome = 'João';
+const sobrenome = 'Silva';
+const idade = 27;
