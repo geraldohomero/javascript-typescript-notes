ebf84fee4d940c6411fd444797fd5abf3fe468b3
geraldohomero <geraldohomero@protonmail.com>
2024-01-29 21:47:46 -0300

    update arrays

--- 24-Arrays/main.js
+++ 24-Arrays/main.js
@@ -20,7 +20,7 @@ console.log(resto);
 
 //                     0          1          2
 //                  0  1  2    0  1  2    0  1  2
-const numeros2 = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+const numeros2 = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]; 
 console.log(numeros2[1][2]) // 6
 console.log(numeros2[0][2]) // 3
 
