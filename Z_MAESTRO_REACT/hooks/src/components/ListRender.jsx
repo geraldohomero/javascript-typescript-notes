9a76e63238fa83265c4b11f1d90b0ff1bac8c76b
geraldohomero <geraldohomero@protonmail.com>
2024-03-27 11:00:14 -0300

    Update add Hooks key and ids

--- Z_MAESTRO_REACT/hooks/src/components/ListRender.jsx
+++ Z_MAESTRO_REACT/hooks/src/components/ListRender.jsx
@@ -2,12 +2,26 @@ import { useState } from 'react'
 
 const ListRender = () => {
   const [list] = useState(["Wellerson", "Joberson", "Killerson", "Emmanuellerson"])
+
+  const [users] = useState([
+    { id: 1, name: "Wellerson", age: 25 },
+    { id: 2, name: "Joberson", age: 26 },
+    { id: 3, name: "Killerson", age: 27 },
+    { id: 4, name: "Emmanuellerson", age: 28 },
+  ]);
   return (
     <div>
       <ul>
         {list.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
+      </ul>
+      <ul>
+        {users.map((user => (
+          <li key={user.id}>
+            {user.name} - {user.age} anos
+          </li>
+        )))}
       </ul>
     </div >
   )
