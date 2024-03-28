41e08c69e1a465b5fb6c76c710f1561d9b5a5820
geraldohomero <geraldohomero@protonmail.com>
2024-03-28 13:53:57 -0300

    update

--- Z_MAESTRO_REACT/hooks/src/components/ConditionalRender.jsx
+++ Z_MAESTRO_REACT/hooks/src/components/ConditionalRender.jsx
@@ -3,13 +3,29 @@ import { useState } from 'react'
 
 const ConditionalRender = () => {
 
-  const [x] = useState(0)
+  const [x] = useState(false);
+  const [name, setName] = useState("Jeremias")
 
   return (
     <div>
-
+      <h1>Isso será exibido?</h1>
+      {x && <h2>Sim</h2>}
+      {!x && <h2>Não</h2>}
+      <h1> If ternário</h1>
+      {name === "Valdermeirsson" ? (
+        <div>
+          <h2>Olá Valdermeirsson</h2>
+        </div>
+      ) : (
+        <div>
+          <h2>Olá estranho</h2>
+        </div>
+      )}
+      {/* Re-renderização dos componentes */}
+      <button onClick={() => setName("Valdermeirsson")}>Clica Aqui</button>
     </div>
   )
 }
 
-export default ConditionalRender
+
+export default ConditionalRender;
--- Z_MAESTRO_REACT/hooks/vite.config.js
+++ Z_MAESTRO_REACT/hooks/vite.config.js
@@ -5,9 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-
-{
-  esbuild: {
-    jsxInject: (str: string) => !str.includes('import React') ? "import React from 'react'" : ""
-  }
-}
